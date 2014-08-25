532303ec1706dba696c66f34a58beb5a5d47821d
TonyAdo <coolhzb@163.com>
2014-08-25 15:58:51 +0800

    refactor SpecificJsonWriter....

--- baiji-core/src/main/java/com/ctriposs/baiji/io/JsonEncoder.java
+++ baiji-core/src/main/java/com/ctriposs/baiji/io/JsonEncoder.java
@@ -4,49 +4,95 @@ import com.ctriposs.baiji.schema.Schema;
 import org.codehaus.jackson.JsonEncoding;
 import org.codehaus.jackson.JsonFactory;
 import org.codehaus.jackson.JsonGenerator;
+import org.codehaus.jackson.util.DefaultPrettyPrinter;
+import org.codehaus.jackson.util.MinimalPrettyPrinter;
 
 import java.io.IOException;
 import java.io.OutputStream;
 
 public class JsonEncoder implements Encoder {
+    private static final String LINE_SEPARATOR = System.getProperty("line.separator");
 
     private JsonGenerator out;
 
     public JsonEncoder(Schema sc, OutputStream out) throws IOException {
-        this(sc, getJsonGenerator(out));
+        this(sc, getJsonGenerator(out, false));
+    }
+
+    public JsonEncoder(Schema sc, OutputStream out, boolean pretty) throws IOException {
+        this(sc, getJsonGenerator(out, pretty));
     }
 
     public JsonEncoder(Schema sc, JsonGenerator out) {
         this.out = out;
     }
 
-    private static JsonGenerator getJsonGenerator(OutputStream out) throws IOException {
-        return out == null ? null :
-          new JsonFactory().createJsonGenerator(out, JsonEncoding.UTF8);
+    private static JsonGenerator getJsonGenerator(OutputStream out, boolean pretty) throws IOException {
+        if (null == out)
+            throw new NullPointerException("OutputStream can't be null");
+        JsonGenerator g = new JsonFactory().createJsonGenerator(out, JsonEncoding.UTF8);
+        if (pretty) {
+            DefaultPrettyPrinter pp = new DefaultPrettyPrinter() {
+                //@Override
+                public void writeRootValueSeparator(JsonGenerator jg)
+                        throws IOException
+                {
+                    jg.writeRaw(LINE_SEPARATOR);
+                }
+            };
+            g.setPrettyPrinter(pp);
+        } else {
+            MinimalPrettyPrinter pp = new MinimalPrettyPrinter();
+            pp.setRootValueSeparator(LINE_SEPARATOR);
+            g.setPrettyPrinter(pp);
+        }
+        return g;
+    }
+
+    /**
+     * Write the Start Object
+     * @throws IOException
+     */
+    public void writeStartObject() throws IOException {
+        out.writeStartObject();
+    }
+
+    /**
+     * Write the End Object
+     * @throws IOException
+     */
+    public void writeEndObject() throws IOException {
+        out.writeEndObject();
     }
 
     @Override
     public void writeNull() throws IOException {
+        out.writeNull();
     }
 
     @Override
     public void writeBoolean(boolean b) throws IOException {
+        out.writeBoolean(b);
     }
 
     @Override
     public void writeInt(int n) throws IOException {
+        out.writeNumber(n);
     }
 
     @Override
     public void writeLong(long n) throws IOException {
+        out.writeNumber(n);
     }
 
     @Override
     public void writeFloat(float f) throws IOException {
+        out.writeNumber(f);
     }
 
     @Override
     public void writeDouble(double d) throws IOException {
+        out.writeNumber(d);
     }
 
     @Override
--- baiji-core/src/main/java/com/ctriposs/baiji/specific/SpecificJsonWriter.java
+++ baiji-core/src/main/java/com/ctriposs/baiji/specific/SpecificJsonWriter.java
@@ -1,64 +1,34 @@
 package com.ctriposs.baiji.specific;
 
-import com.ctriposs.baiji.exception.BaijiTypeException;
-import com.ctriposs.baiji.generic.PreresolvingDatumWriter;
+import com.ctriposs.baiji.generic.DatumWriter;
 import com.ctriposs.baiji.io.Encoder;
-import com.ctriposs.baiji.schema.EnumSchema;
 import com.ctriposs.baiji.schema.RecordSchema;
 import com.ctriposs.baiji.schema.Schema;
 
 import java.io.IOException;
-import java.util.List;
 
-public class SpecificJsonWriter<T> extends PreresolvingDatumWriter<T> {
+public class SpecificJsonWriter<T> implements DatumWriter<T> {
 
+    private Schema root;
 
-    public SpecificJsonWriter(Schema schema) {
-        super(schema, new SpecificArrayAccess(), new DefaultMapAccess());
+    public SpecificJsonWriter(Schema root) {
+        this.root = root;
     }
 
     @Override
-    protected void ensureRecordObject(RecordSchema recordSchema, Object value) {
-    }
-
-    @Override
-    protected void writeRecordFields(Object record, RecordFieldWriter[] writers, Encoder encoder) throws IOException {
-    }
-
-    @Override
-    protected void writeField(Object record, String fieldName, int fieldPos, ItemWriter writer, Encoder encoder) throws IOException {
-    }
-
-    @Override
-    protected ItemWriter resolveEnum(EnumSchema es) {
+    public Schema getSchema() {
         return null;
     }
 
     @Override
-    protected boolean unionBranchMatches(Schema sc, Object obj) {
-        return false;
+    public void write(T datum, Encoder out) throws IOException {
+        write(root, datum, out);
     }
 
-    private static class SpecificArrayAccess implements ArrayAccess {
-        @Override
-        public void ensureArrayObject(Object value) {
-            if (!(value instanceof List)) {
-                throw new BaijiTypeException("Array does not implement non-generic IList");
-            }
-        }
+    /** Called to write record data.*/
+    protected void write(Schema schema, Object datum, Encoder out) {
+        if (schema instanceof RecordSchema) {
 
-        @Override
-        public long getArrayLength(Object value) {
-            return ((List) value).size();
-        }
-
-        @Override
-        public void writeArrayValues(Object array, ItemWriter valueWriter, Encoder encoder)
-                throws IOException {
-            List list = (List) array;
-            for (int i = 0; i < list.size(); i++) {
-                valueWriter.write(list.get(i), encoder);
-            }
         }
     }
 }
