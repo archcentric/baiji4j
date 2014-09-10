7bdd86648b74a1c9f8600568582597b3c429bce8
CH3CHO <kentstl@163.com>
2014-09-10 09:50:45 +0800

    Add service registration in MovieService.

--- baiji-rpc-samples/movie-service/src/main/java/com/ctriposs/baiji/rpc/samples/movie/StartServer.java
+++ baiji-rpc-samples/movie-service/src/main/java/com/ctriposs/baiji/rpc/samples/movie/StartServer.java
@@ -1,9 +1,13 @@
 package com.ctriposs.baiji.rpc.samples.movie;
 
+import com.ctriposs.baiji.rpc.common.BaijiContract;
 import com.ctriposs.baiji.rpc.server.BaijiHttpRequestRouter;
 import com.ctriposs.baiji.rpc.server.HttpRequestRouter;
 import com.ctriposs.baiji.rpc.server.ServiceConfig;
 import com.ctriposs.baiji.rpc.server.netty.BlockingHttpServerBuilder;
+import com.ctriposs.baiji.rpc.server.registry.EtcdServiceRegistry;
+import com.ctriposs.baiji.rpc.server.registry.ServiceInfo;
+import com.ctriposs.baiji.rpc.server.registry.ServiceRegistry;
 import io.netty.channel.ChannelOption;
 
 public final class StartServer {
@@ -20,5 +24,14 @@ public final class StartServer {
                 .serverSocketOption(ChannelOption.SO_BACKLOG, 100)
                 .clientSocketOption(ChannelOption.TCP_NODELAY, true)
                 .build().startWithoutWaitingForShutdown();
+
+        ServiceRegistry registry = new EtcdServiceRegistry("http://localhost:4001/");
+        BaijiContract contract = MovieService.class.getAnnotation(BaijiContract.class);
+        ServiceInfo service = new ServiceInfo.Builder().serviceName(contract.serviceName())
+                .serviceNamespace(contract.serviceNamespace())
+                .subEnv("dev")
+                .port(8112).build();
+        registry.addService(service);
+        registry.run();
     }
 }
