ff18dd52eab7468f9281de7018299900f74cf3fd
TonyAdo <coolhzb@163.com>
2014-10-21 17:22:48 +0800

    update the fast json performance test ....

--- baiji-core/src/test/java/com/ctriposs/baiji/fastjson/FastJsonSerializerPerfTest.java
+++ baiji-core/src/test/java/com/ctriposs/baiji/fastjson/FastJsonSerializerPerfTest.java
@@ -22,49 +22,49 @@ public class FastJsonSerializerPerfTest {
 
     @Test
     public void testDeserialize_10tiny() throws Exception {
-        testDeserialize("/t10records.json", 10, 5, "with tiny data");
+        testDeserialize("/t10records.json", 10, 1, "with tiny data");
     }
 
-    @Test
+    /*@Test
     public void testDeserialize_50tiny() throws Exception {
-        testDeserialize("/t50records.json", 50, 5, "with tiny data");
+        testDeserialize("/t50records.json", 50, 50, "with tiny data");
     }
 
     @Test
     public void testDeserialize_300tiny() throws Exception {
-        testDeserialize("/t300records.json", 300, 5, "with tiny data");
+        testDeserialize("/t300records.json", 300, 50, "with tiny data");
     }
 
     @Test
     public void testDeserialize_10big() throws Exception {
-        testDeserialize("/b10records.json", 10, 5, "with big data");
+        testDeserialize("/b10records.json", 10, 50, "with big data");
     }
 
     @Test
     public void testDeserialize_50big() throws Exception {
-        testDeserialize("/b50records.json", 50, 5, "with big data");
+        testDeserialize("/b50records.json", 50, 50, "with big data");
     }
 
     @Test
     public void testDeserialize_300big() throws Exception {
-        testDeserialize("/b300records.json", 300, 5, "with big data");
-    }
+        testDeserialize("/b300records.json", 300, 50, "with big data");
+    }*/
 
     protected void testDeserialize(String fileName, int size, int loop, String dataType) {
         List<Long> results = new ArrayList<Long>();
         for (int i = 0; i < loop; i++) {
             try (InputStream in = FastJsonSerializerPerfTest.class.getResourceAsStream(fileName)) {
-                long start = System.currentTimeMillis();
+                long start = System.nanoTime();
                 TestSerializerSampleList sample = deserialize(TestSerializerSampleList.class, in);
-                long end = System.currentTimeMillis();
+                long end = System.nanoTime();
                 results.add(end - start);
             } catch (IOException e) {/**/}
         }
 
         long[] pef = readResults(results);
-        System.out.println("parse " + size + " records (" + loop + " times)" + dataType + " : " + pef[0] / size + " (min) ms/op\n");
-        System.out.println("parse " + size + " records (" + loop + " times)" + dataType + " : " + pef[1] / size + " (max) ms/op\n");
-        System.out.println("parse " + size + " records (" + loop + " times)" + dataType + " : " + pef[2] / size + " (avg) ms/op\n");
+        System.out.println("parse " + size + " records (" + loop + " times)" + dataType + " : " + pef[0] / size + " (min) ns/op\n");
+        System.out.println("parse " + size + " records (" + loop + " times)" + dataType + " : " + pef[1] / size + " (max) ns/op\n");
+        System.out.println("parse " + size + " records (" + loop + " times)" + dataType + " : " + pef[2] / size + " (avg) ns/op\n");
     }
 
     private long[] readResults(List<Long> results) {
