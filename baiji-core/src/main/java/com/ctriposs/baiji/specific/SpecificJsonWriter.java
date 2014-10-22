867666dc9778ce8109abb422d451b9a0629afa75
TonyAdo <coolhzb@163.com>
2014-10-22 11:17:22 +0800

    SpecificJsonWriter update....

--- baiji-core/src/main/java/com/ctriposs/baiji/specific/SpecificJsonWriter.java
+++ baiji-core/src/main/java/com/ctriposs/baiji/specific/SpecificJsonWriter.java
@@ -50,7 +50,6 @@ public final class SpecificJsonWriter<T> {
                     RecordSchema recordSchema = (RecordSchema) schema;
                     JsonGenerator g = FACTORY.createJsonGenerator(os, JsonEncoding.UTF8);
                     _writerCache.get(recordSchema.getType()).write(recordSchema, obj, g);
-                    //writeRecord(recordSchema, obj, g);
                     g.flush();
                     g.close();
                 } catch (Exception e) {
@@ -61,131 +60,6 @@ public final class SpecificJsonWriter<T> {
             }
         } else {
             throw new BaijiRuntimeException("schema must be RecordSchema");
-        }
-    }
-
-    /** Called to write record.*/
-    protected void writeRecord(RecordSchema recordSchema, Object datum, JsonGenerator generator) throws IOException {
-        List<Field> fields = recordSchema.getFields();
-        generator.writeStartObject();
-        for (Field field : fields) {
-            Object value = ((SpecificRecord) datum).get(field.getPos());
-            if (value == null)
-                continue;
-            generator.writeFieldName(field.getName());
-            writeValue(field.getSchema(), value, generator);
-        }
-        generator.writeEndObject();
-    }
-
-    /** Called to write value.*/
-    private void writeValue(Schema fieldSchema, Object datum, JsonGenerator generator) throws IOException {
-        try {
-            switch (fieldSchema.getType()) {
-                case INT:
-                    generator.writeNumber((Integer) datum);
-                    break;
-                case LONG:
-                    generator.writeNumber((Long) datum);
-                    break;
-                case DOUBLE:
-                    generator.writeNumber((Double) datum);
-                    break;
-                case FLOAT:
-                    generator.writeNumber((Float) datum);
-                    break;
-                case BOOLEAN:
-                    generator.writeBoolean((Boolean) datum);
-                    break;
-                case STRING:
-                    generator.writeString(datum.toString());
-                    break;
-                case BYTES:
-                    writeBytes(datum, generator);
-                    break;
-                case NULL:
-                    generator.writeNull();
-                    break;
-                case DATETIME:
-                    generator.writeNumber(((Calendar) datum).getTimeInMillis());
-                    break;
-                case RECORD:
-                    writeRecord((RecordSchema) fieldSchema, datum, generator);
-                    break;
-                case ENUM:
-                    writeEnum((EnumSchema) fieldSchema, (Enum) datum, generator);
-                    break;
-                case ARRAY:
-                    writeArray((ArraySchema) fieldSchema, (List) datum, generator);
-                    break;
-                case MAP:
-                    writeMap((MapSchema) fieldSchema, (Map) datum, generator);
-                    break;
-                case UNION:
-                    writeUnion((UnionSchema) fieldSchema, datum, generator);
-                    break;
-                default:
-                    error(fieldSchema, datum);
-                    break;
-            }
-        } catch (NullPointerException e) {
-            throw npe(e, " of "+ fieldSchema.getName());
-        }
-    }
-
-    /** Called to write bytes.*/
-    private void writeBytes(Object datum, JsonGenerator generator) throws IOException {
-        byte[] bytes = (byte[]) datum;
-        generator.writeBinary(bytes, 0, bytes.length);
-    }
-
-    /** Called to write enum.*/
-    private void writeEnum(EnumSchema enumSchema, Enum en, JsonGenerator generator) throws IOException {
-        String value = enumSchema.get(en.ordinal());
-        generator.writeString(value);
-    }
-
-    /** Called to write array.*/
-    private void writeArray(ArraySchema arraySchema, List array, JsonGenerator generator) throws IOException {
-        Schema itemSchema = arraySchema.getItemSchema();
-        generator.writeStartArray();
-        for (Iterator iterator = getArrayElements(array); iterator.hasNext();) {
-            writeValue(itemSchema, iterator.next(), generator);
-        }
-        generator.writeEndArray();
-    }
-
-    /** Called by {@link #writeArray} to enumerate array elements.*/
-    private Iterator getArrayElements(List array) {
-        return array.iterator();
-    }
-
-    /** Called to write map.*/
-    private void writeMap(MapSchema mapSchema, Map map, JsonGenerator generator) throws IOException {
-        Schema valueSchema = mapSchema.getValueSchema();
-        Iterable<Map.Entry<Object, Object>> iterable = getMapEntries(map);
-        generator.writeStartObject();
-        for (Map.Entry<Object, Object> entry : iterable) {
-            generator.writeFieldName(entry.getKey().toString());
-            writeValue(valueSchema, entry.getValue(), generator);
-        }
-        generator.writeEndObject();
-    }
-
-    /** Called by {@link #writeMap} to enumerate map elements.*/
-    private Iterable<Map.Entry<Object, Object>> getMapEntries(Map map) {
-        return map.entrySet();
-    }
-
-    /** Called to write union.*/
-    private void writeUnion(UnionSchema unionSchema, Object datum, JsonGenerator generator) throws IOException {
-        int size = unionSchema.size();
-        for (int i = 0; i < size; i++) {
-            if (unionSchema.get(i).getType() == SchemaType.NULL)
-                continue;
-
-            writeValue(unionSchema.get(i), datum, generator);
-            return;
         }
     }
 
