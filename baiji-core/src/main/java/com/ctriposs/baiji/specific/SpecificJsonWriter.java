bccc7b61752c861a0be48dc6dbebe909e4e0f6bf
TonyAdo <coolhzb@163.com>
2014-10-22 10:29:30 +0800

    little update.....

--- baiji-core/src/main/java/com/ctriposs/baiji/specific/SpecificJsonWriter.java
+++ baiji-core/src/main/java/com/ctriposs/baiji/specific/SpecificJsonWriter.java
@@ -11,13 +11,30 @@ import org.codehaus.jackson.JsonParser;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 
 public final class SpecificJsonWriter<T> {
 
-    static final JsonFactory FACTORY = new JsonFactory();
+    private static final JsonFactory FACTORY = new JsonFactory();
+    private static final ConcurrentMap<SchemaType, JsonWritable> _writerCache = new ConcurrentHashMap<>();
 
     static {
         FACTORY.enable(JsonParser.Feature.ALLOW_COMMENTS);
+        _writerCache.put(SchemaType.INT, new IntegerWriter());
+        _writerCache.put(SchemaType.LONG, new LongWriter());
+        _writerCache.put(SchemaType.DOUBLE, new DoubleWriter());
+        _writerCache.put(SchemaType.FLOAT, new FloatWriter());
+        _writerCache.put(SchemaType.BOOLEAN, new BooleanWriter());
+        _writerCache.put(SchemaType.STRING, new StringWriter());
+        _writerCache.put(SchemaType.BYTES, new BytesWriter());
+        _writerCache.put(SchemaType.NULL, new NullWriter());
+        _writerCache.put(SchemaType.DATETIME, new DatetimeWriter());
+        _writerCache.put(SchemaType.RECORD, new RecordWriter());
+        _writerCache.put(SchemaType.ENUM, new EnumWriter());
+        _writerCache.put(SchemaType.ARRAY, new ArrayWriter());
+        _writerCache.put(SchemaType.MAP, new MapWriter());
+        _writerCache.put(SchemaType.UNION,new UnionWriter());
     }
 
     /**
@@ -32,10 +49,11 @@ public final class SpecificJsonWriter<T> {
                 try {
                     RecordSchema recordSchema = (RecordSchema) schema;
                     JsonGenerator g = FACTORY.createJsonGenerator(os, JsonEncoding.UTF8);
+                    _writerCache.get(recordSchema.getType()).write(recordSchema, obj, g);
                     writeRecord(recordSchema, obj, g);
                     g.flush();
                     g.close();
-                } catch (IOException e) {
+                } catch (Exception e) {
                     throw new BaijiRuntimeException("Serialize process failed.", e);
                 }
             } else {
@@ -187,7 +205,7 @@ public final class SpecificJsonWriter<T> {
         void write(Schema schema, Object datum, JsonGenerator g) throws Exception;
     }
 
-    private class IntegerWriter implements JsonWritable {
+    private static class IntegerWriter implements JsonWritable {
 
         @Override
         public void write(Schema schema, Object datum, JsonGenerator g) throws Exception {
@@ -195,7 +213,7 @@ public final class SpecificJsonWriter<T> {
         }
     }
 
-    private class LongWriter implements JsonWritable {
+    private static class LongWriter implements JsonWritable {
 
         @Override
         public void write(Schema schema, Object datum, JsonGenerator g) throws Exception {
@@ -203,7 +221,7 @@ public final class SpecificJsonWriter<T> {
         }
     }
 
-    private class DoubleWriter implements JsonWritable {
+    private static class DoubleWriter implements JsonWritable {
 
         @Override
         public void write(Schema schema, Object datum, JsonGenerator g) throws Exception {
@@ -211,7 +229,7 @@ public final class SpecificJsonWriter<T> {
         }
     }
 
-    private class FloatWriter implements JsonWritable {
+    private static class FloatWriter implements JsonWritable {
 
         @Override
         public void write(Schema schema, Object datum, JsonGenerator g) throws Exception {
@@ -219,7 +237,7 @@ public final class SpecificJsonWriter<T> {
         }
     }
 
-    private class BooleanWriter implements JsonWritable {
+    private static class BooleanWriter implements JsonWritable {
 
         @Override
         public void write(Schema schema, Object datum, JsonGenerator g) throws Exception {
@@ -227,7 +245,7 @@ public final class SpecificJsonWriter<T> {
         }
     }
 
-    private class StringWriter implements JsonWritable {
+    private static class StringWriter implements JsonWritable {
 
         @Override
         public void write(Schema schema, Object datum, JsonGenerator g) throws Exception {
@@ -235,12 +253,120 @@ public final class SpecificJsonWriter<T> {
         }
     }
 
-    private class BytesWriter implements JsonWritable {
+    private static class BytesWriter implements JsonWritable {
 
         @Override
         public void write(Schema schema, Object datum, JsonGenerator g) throws Exception {
             byte[] bytes = (byte[]) datum;
             g.writeBinary(bytes);
+        }
+    }
+
+    private static class NullWriter implements JsonWritable {
+
+        @Override
+        public void write(Schema schema, Object datum, JsonGenerator g) throws Exception {
+            g.writeNull();
+        }
+    }
+
+    private static class DatetimeWriter implements JsonWritable {
+
+        @Override
+        public void write(Schema schema, Object datum, JsonGenerator g) throws Exception {
+            g.writeNumber(((Calendar) datum).getTimeInMillis());
+        }
+    }
+
+    private static class RecordWriter implements JsonWritable {
+
+        @Override
+        public void write(Schema schema, Object datum, JsonGenerator g) throws Exception {
+            RecordSchema recordSchema = (RecordSchema) schema;
+            List<Field> fields = recordSchema.getFields();
+            g.writeStartObject();
+            for (Field field : fields) {
+                Object value = ((SpecificRecord) datum).get(field.getPos());
+                if (value == null)
+                    continue;
+                g.writeFieldName(field.getName());
+                Schema fieldSchema = field.getSchema();
+                _writerCache.get(fieldSchema.getType()).write(fieldSchema, value, g);
+            }
+            g.writeEndObject();
+        }
+    }
+
+    private static class EnumWriter implements JsonWritable {
+
+        @Override
+        public void write(Schema schema, Object datum, JsonGenerator g) throws Exception {
+            EnumSchema enumSchema = (EnumSchema) schema;
+            Enum en = (Enum) datum;
+            g.writeString(enumSchema.get(en.ordinal()));
+        }
+    }
+
+    private static class ArrayWriter implements JsonWritable {
+
+        private Iterator getArrayElements(List array) {
+            return array.iterator();
+        }
+
+        @Override
+        public void write(Schema schema, Object datum, JsonGenerator g) throws Exception {
+            Schema itemSchema = ((ArraySchema) schema).getItemSchema();
+            JsonWritable itemWriter = _writerCache.get(itemSchema.getType());
+            List array = (List) datum;
+            g.writeStartArray();
+            for (Iterator iterator = getArrayElements(array); iterator.hasNext();) {
+                itemWriter.write(itemSchema, iterator.next(), g);
+            }
+            g.writeEndArray();
+        }
+    }
+
+    private static class MapWriter implements JsonWritable {
+
+        @SuppressWarnings("unchecked")
+        private Iterable<Map.Entry<Object, Object>> getMapEntries(Map map) {
+            return map.entrySet();
+        }
+
+        @Override
+        public void write(Schema schema, Object datum, JsonGenerator g) throws Exception {
+            Schema valueSchema = ((MapSchema) schema).getValueSchema();
+            JsonWritable valueWriter = _writerCache.get(valueSchema.getType());
+            Map map = (Map) datum;
+            Iterable<Map.Entry<Object, Object>> iterable = getMapEntries(map);
+            g.writeStartObject();
+            for (Map.Entry<Object, Object> entry : iterable) {
+                g.writeFieldName(entry.getKey().toString());
+                valueWriter.write(valueSchema, entry.getValue(), g);
+            }
+            g.writeEndObject();
+        }
+    }
+
+    private static class UnionWriter implements JsonWritable {
+
+        @Override
+        public void write(Schema schema, Object datum, JsonGenerator g) throws Exception {
+            UnionSchema unionSchema = (UnionSchema) schema;
+            int size = unionSchema.size();
+
+            if (size != 2) {
+                return;
+            }
+
+            for (int i = 0; i < size; i++) {
+                Schema itemSchema = unionSchema.get(i);
+                if (itemSchema.getType() == SchemaType.NULL)
+                    continue;
+
+                _writerCache.get(itemSchema.getType()).write(itemSchema, datum, g);
+                return;
+            }
         }
     }
 }
